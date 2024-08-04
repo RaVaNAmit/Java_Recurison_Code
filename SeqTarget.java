841300712d2dbcd807932c7e931fd383f5865eec
RaVaNAmit <amit.smriti.1999@gmail.com>
2024-08-04 22:00:42 +0530

    Merge Conflicts checks

---
+++ SeqTarget.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class SeqTarget {
+
+
+  public static void target_seq(int index , int arr1[] , ArrayList<Integer> store){
+
+  }
+  
+  public static void main(String[] args) {
+    
+    int arr[] = {1 , 2 , 1};
+
+    target_seq(0 , arr , new ArrayList<>());
+  
+  }
+
+}
