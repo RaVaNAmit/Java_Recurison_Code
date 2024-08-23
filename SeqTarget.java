c891ea73fe2a16cba54b690be07ba91a34ec93fe
RaVaNAmit <amit.smriti.1999@gmail.com>
2024-08-23 20:43:44 +0530

    betu name edit

--- SeqTarget.java
+++ SeqTarget.java
@@ -1,17 +1,29 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class SeqTarget {
 
 
-  public static void target_seq(int index , int arr1[] , ArrayList<Integer> store){
+  public static void target_seq(int index , int arr1[] , ArrayList<Integer> store , int target){
+    
+
+    System.out.println("Sandra Darwin and Kaju Darwin");
+
 
   }
   
   public static void main(String[] args) {
     
-    int arr[] = {1 , 2 , 1};
+    Scanner sc = new Scanner(System.in);
 
-    target_seq(0 , arr , new ArrayList<>());
+    int arr[] = { 3 , 1  , 2 };
+
+    ArrayList<Integer> store = new ArrayList<>();
+
+    int target = sc.nextInt();
+
+    target_seq(0 , arr ,  store, target);
+
   
   }
 
